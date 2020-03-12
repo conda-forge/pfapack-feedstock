c810c089dd4d1edc5c71c16d7d6c0679bf0b7065
Bas Nijholt <basnijholt@gmail.com>
2020-03-12 14:47:43 +0100

    add rpath

--- recipe/Makefile.C
+++ recipe/Makefile.C
@@ -2,7 +2,7 @@ OBJECTS = skpfa.o skpf10.o skbpfa.o skbpf10.o sktrf.o sktrd.o skbtrd.o
 
 all: $(OBJECTS)
 	$(AR) rvu libcpfapack.a $(OBJECTS)
-	$(CC) -shared $(CFLAGS) -o libcpfapack.so $(OBJECTS) -L${PREFIX}/lib -lpfapack -llapack -lblas
+	$(CC) -shared $(CFLAGS) -o libcpfapack.so $(OBJECTS) -L${PREFIX}/lib -lpfapack -Wl,-rpath,${PREFIX}/lib -llapack -lblas
 
 clean:
 	rm -f *.o
