f7c9a1f76253ed4229e6137954469f744b439364
Anton Akhmerov <anton.akhmerov@gmail.com>
2025-10-13 13:05:35 +0200

    try to fix windows builds

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,4 +4,10 @@ setlocal enabledelayedexpansion
 set PIP_NO_BUILD_ISOLATION=1
 set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
 
+if "%MESON_ARGS%"=="" (
+	set "MESON_ARGS=-Dfortran_std=none"
+) else (
+	set "MESON_ARGS=%MESON_ARGS% -Dfortran_std=none"
+)
+
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
