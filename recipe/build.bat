7474ad077b536e1ff29fc07a2dcdc0cb2d874d32
Anton Akhmerov <anton.akhmerov@gmail.com>
2025-10-13 13:49:19 +0200

    try using lapack on windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,10 +4,14 @@ setlocal enabledelayedexpansion
 set PIP_NO_BUILD_ISOLATION=1
 set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
 
+for %%I in ("%LIBRARY_LIB%") do (
+	set "OPENBLAS_LIBDIR=%%~fI"
+)
+
 if "%MESON_ARGS%"=="" (
-	set "MESON_ARGS=-Dfortran_std=none"
+	set "MESON_ARGS=-Dfortran_std=none -Dopenblas_libdir=!OPENBLAS_LIBDIR!"
 ) else (
-	set "MESON_ARGS=%MESON_ARGS% -Dfortran_std=none"
+	set "MESON_ARGS=%MESON_ARGS% -Dfortran_std=none -Dopenblas_libdir=!OPENBLAS_LIBDIR!"
 )
 
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
